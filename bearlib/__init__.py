32f40b043229402f249073c2fe38254004b8e81f
Mike Taylor <bear42@gmail.com>
2013-03-08 04:55:01 -0500

    brought over some daemon related tools greatly simplified and refactored the config parsing and log setup routines

--- bearlib/__init__.py
+++ bearlib/__init__.py
@@ -17,130 +17,168 @@ __license__   = 'BSD 2-Clause'
 __site__      = 'https://github.com/bear/bearlib'
 __version__   = u'.'.join(map(str, VERSION[0:3])) + u''.join(VERSION[3:])
 
+
 import os
-import os.path
 import sys
-import json
+import pwd
+import grp
 import types
+import signal
 import logging
 
 from optparse import OptionParser
+from ConfigParser import SafeConfigParser
+
 
 _ourPath = os.getcwd()
-_ourName = os.path.splitext(os.path.basename(sys.argv[0]))[0]
-log      = logging.getLogger(_ourName)
+log      = logging.getLogger('babble')
 
-def bLogs(logger, echo=True, chatty=False, debug=False, loglevel=logging.INFO, logpath=None, logname=None, fileHandler=None):
-    """ Initialize logging
-    """
-    if logpath is not None:
-        if logname is None:
-            logname = _ourName
+def pidWrite(pidFile):
+    os.umask(077) # set umask for pid
 
-        if fileHandler is None:
-            _handler      = logging.FileHandler(os.path.join(logpath, logname))
-            fileFormatter = logging.Formatter('%(asctime)s %(levelname)-7s %(processName)s: %(message)s')
+    with open(pidFile, "w") as f:
+        f.write(str(os.getpid()))
 
-            _handler.setFormatter(fileFormatter)
-            logger.addHandler(_handler)
-            logger.fileHandler = _handler
-        else:
-            logger.addHandler(fileHandler)
-            logger.fileHandler = fileHandler
+def pidRead(pidFile):
+    try:
+        with open(pidFile, 'r') as f:
+            return int(f.read())
+    except IOError:
+        return -1
 
-    if echo:
-        echoHandler = logging.StreamHandler()
+def pidClear(pidFile):
+    if not isRunning(pidFile):
+        if os.path.exists(pidFile):
+            os.remove(pidFile)
 
-        if chatty:
-            echoFormatter = logging.Formatter('%(asctime)s %(levelname)-7s %(processName)s[%(process)d]: %(message)s')
-        else:
-            echoFormatter = logging.Formatter('%(asctime)s %(levelname)-7s %(message)s')
+def isRunning(pidFile):
+    pid = pidRead(pidFile)
 
-        echoHandler.setFormatter(echoFormatter)
-
-        logger.addHandler(echoHandler)
-
-    if debug:
-        logger.setLevel(loglevel)
+    if pid == -1:
+        return False
     else:
-        logger.setLevel(logging.DEBUG)
+        try:
+            os.kill(pid, 0)
+            return True
+        except OSError:
+            log.warn('pid %d for %s found.' % (pid, pidFile))
+            return False
 
-# HACK
-# yes, I am loading a custom log config right after defining the helper routine
-# that does the log init
-# I have not found a way to enable console echoing during the config item creation
-# in a way that doesn't completely stink up the room!
-bLogs(log, echo=True, debug=True)
+def shutdownHandler(signal, frame):
+    log.info('shutdown requested')
+    sys.exit(0)
 
-class bConfig(object):
-    def __init__(self, configFilename=None):
-        """ Parse command line parameters and populate the options object
-        """
-        self.log            = logging.getLogger(_ourName)
-        self.options        = None
-        self.appPath        = _ourPath
-        self.configFilename = configFilename
-        self.config         = {}
+def daemonize(config):
+    if not hasattr(config, 'pidfile'):
+        raise Exception('pidfile is a required configuration item')
+    if not hasattr(config, 'uid'):
+        raise Exception('uid is a required configuration item')
+    if not hasattr(config, 'gid'):
+        raise Exception('gid is a required configuration item')
 
-        # these are my normal defaults
-        self._defaults = { 'configFile':  ('-c', '--config',  self.configFilename, 'Configuration Filename (optionally w/path'),
-                           'debug':       ('-d', '--debug',   False,               'Enable Debug'),
-                           'echo':        ('-e', '--echo',    False,               'Enable log echo to the console'),
-                           'logpath':     ('-l', '--logpath', '',                  'Path where log file is to be written'),
-                           'logfile':     ('',   '--logfile', '%s.log' % _ourName, 'log filename'),
-                           'verbose':     ('-v', '--verbose', False,               'show extra output from remote commands'),
-                         }
+    if isRunning(config.pidfile):
+        log.error('Babble is still running. Exiting')
+        sys.exit(1)
+    else:
+        if os.fork() == 0:
+            os.setsid()
 
-    def findConfigFile(self, paths=None, envVar=None):
-        searchPaths = []
+            signal.signal(signal.SIGHUP, signal.SIG_IGN)
 
-        if paths is not None:
-            for path in paths:
-                log.debug('adding %s to the search path' % path)
-                searchPaths.append(path)
+            pid = os.fork()
 
-        for path in (_ourPath, os.path.expanduser('~')):
-            log.debug('adding %s to the search path' % path)
-            searchPaths.append(path)
-        
-        if envVar is not None and envVar in os.environ:
-            path = os.environ[envVar]
-            log.debug('adding %s to the search path' % path)
-            searchPaths.append(path)
-
-        for path in searchPaths:
-            s = os.path.join(path, self.configFilename)
-            if os.path.isfile(s):
-                log.debug('configuation file found: %s' % s)
-                self.options.configFile = s
-
-    def addConfig(self, key, shortCmd='', longCmd='', defaultValue=None, helpText=''):
-        if len(shortCmd) + len(longCmd) == 0:
-            log.error('You must provide either a shortCmd or a longCmd value - both cannot be empty')
-        elif key is None and type(key) is types.StringType:
-            log.error('The configuration key must be a string')
+            if pid != 0:
+                os._exit(0)
+            else:
+                pidClear(config.pidfile)
+                pidWrite(config.pidfile)
         else:
-            self.config[key] = (shortCmd, longCmd, defaultValue, helpText)
+            os._exit(0)
 
-    def load(self, defaults=None, configPaths=None, configEnvVar=None):
-        parser        = OptionParser()
-        self.defaults = {}
+    signal.signal(signal.SIGINT,  shutdownHandler)
+    signal.signal(signal.SIGTERM, shutdownHandler)
 
-        if defaults is not None:
-            for key in defaults:
-                self.defaults[key] = defaults[key]
+    if os.getuid() != 0:
+        return
 
-        # load my config items, but just in case the caller has other ideas
-        # do not load them if the key is already present
-        # TODO need to add some way to also cross check short/long command values
-        for key in self._defaults:
-            if key not in self.config:
-                self.config[key] = self._defaults[key]
+    uid = pwd.getpwnam(config.uid).pw_uid
+    gid = grp.getgrnam(config.gid).gr_gid
 
-        for key in self.config:
-            items = self.config[key]
+    os.setgroups([])
 
-            (shortCmd, longCmd, defaultValue, helpText) = items
+    os.setgid(gid)
+    os.setuid(uid)
+
+    os.umask(077)
+
+def relativeDelta(td):
+    s = ''
+    if td.days < 0:
+        t = "%s ago"
+    else:
+        t = "in %s"
+
+    days    = abs(td.days)
+    seconds = abs(td.seconds)
+    minutes = seconds / 60
+    hours   = minutes / 60
+    weeks   = days / 7
+    months  = days / 30
+    years   = days / 365
+
+    if days == 0:
+        if seconds < 20:
+            s = 'just now'
+        if seconds < 60:
+            s = '%d seconds' % seconds
+            s = t % s
+        if seconds < 120:
+            s = t % 'a minute'
+        if seconds < 3600:
+            s = '%d minutes' % minutes
+            s = t % s
+        if seconds < 7200:
+            s = t % 'an hour'
+        if seconds < 86400:
+            s = '%d hours' % hours
+            s = t % s
+    else:
+        if days == 1:
+            if td.days < 0:
+                s = 'yesterday'
+            else:
+                s = 'tomorrow'
+        elif days < 7:
+            s = '%d days' % days
+            s = t % s
+        elif days < 31:
+            s = '%d weeks' % weeks
+            s = t % s
+        elif days < 365:
+            s = '%d months' % months
+            s = t % s
+        else:
+            s = '%d years' % years
+            s = t % s
+
+    return s
+
+class bConfig():
+    def __init__(self):
+        self.options = None
+        self.args    = None
+
+    def parseCommandLine(self):
+        _config = { 'config':  ('-c', '--config',  'babble.cfg', 'Configuration Filename (optionally w/path'),
+                    'debug':   ('-d', '--debug',   False,        'Enable Debug'),
+                    'logpath': ('-l', '--logpath', '.',          'Path where log file is to be written'),
+                    'logfile': ('',   '--logfile', 'babble.log', 'log filename'),
+                  }
+
+        parser = OptionParser()
+
+        for key in _config:
+            shortCmd, longCmd, defaultValue, helpText = _config[key]
 
             if type(defaultValue) is types.BooleanType:
                 parser.add_option(shortCmd, longCmd, dest=key, action='store_true', default=defaultValue, help=helpText)
@@ -149,25 +187,35 @@ class bConfig(object):
 
         (self.options, self.args) = parser.parse_args()
 
-        if self.options.configFile is not None:
-            self.findConfigFile(configPaths, configEnvVar)
-            self.options.config = self.loadJSON(self.options.configFile)
+        for key in _config:
+            setattr(self, key, getattr(self.options, key))
 
-        bLogs(self.log, echo=getattr(self.options, 'echo', False), 
-                        debug=getattr(self.options, 'debug', False), 
-                        logpath=getattr(self.options, 'logpath', None), 
-                        logname=getattr(self.options, 'logfile', None))
+    def loadConfigFile(self):
+        if os.path.isfile(self.config):
+            cfg = SafeConfigParser()
+            cfg.readfp(open(self.config))
 
-    def loadJson(self, filename):
-        """ Read, parse and return given Json config file
-        """
-        jsonConfig = {}
+            for section in cfg.sections():
+                for key, value in cfg.items(section):
+                    if section == 'babble':
+                        setattr(self, key, value)
+                    else:
+                        if not hasattr(self, section):
+                            setattr(self, section, bConfig())
 
-        if os.path.isfile(filename):
-            try:
-                log.debug('attempting to load json config file [%s]' % filename)
-                jsonConfig = json.loads(' '.join(open(filename, 'r').readlines()))
-            except:
-                log.error('error during loading of config file [%s]' % filename, exc_info=True)
+                        setattr(getattr(self, section), key, value)
 
-        return jsonConfig
+def bLogs(config):
+    if config.logfile is not None:
+        handler       = logging.FileHandler(os.path.join(config.logpath, config.logfile))
+        fileFormatter = logging.Formatter('%(asctime)s %(levelname)-7s %(processName)s: %(message)s')
+
+        handler.setFormatter(fileFormatter)
+        log.addHandler(handler)
+        log.fileHandler = handler
+
+    if config.debug:
+        log.setLevel(logging.DEBUG)
+        log.info('debug logging is enabled')
+    else:
+        log.setLevel(logging.INFO)
