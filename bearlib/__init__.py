2cb2e3b559d8529bc690b04bdd18d6717b698423
bear (Mike Taylor) <bear@andyet.net>
2015-09-23 02:15:33 -0400

    version bump

--- bearlib/__init__.py
+++ bearlib/__init__.py
@@ -6,9 +6,9 @@
 
 __author__       = 'bear (Mike Taylor)'
 __email__        = 'bear@bear.im'
-__copyright__    = 'Copyright (c) 2012 - 2015 Mike Taylor'
+__copyright__    = 'Copyright (c) 2012-2015 Mike Taylor'
 __license__      = 'MIT'
-__version__      = '0.9'
+__version__      = '0.9.1'
 __url__          = 'https://github.com/bear/bearlib'
 __download_url__ = 'https://pypi.python.org/pypi/bearlib'
 __description__  = "bear's toolkit"
